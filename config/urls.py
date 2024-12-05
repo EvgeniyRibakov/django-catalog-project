463f4c31edfc2bcd535b4ae87f365cd2b8bfd87d
EvgeniyRibakov <evgenijrybakov01@gmail.com>
2024-12-05 18:07:40 +0300

    Initial commit: Добавил contact

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('newapp.urls', namespace='newapp'))
+    path('', include('newapp.urls', namespace='newapp')),
+    path('', include('catalog.urls'))
 ]
--- newapp/views.py
+++ newapp/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
 # Create your views here.
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
