6bb275416ead8d4bf7d33b4f9d60c89013008899
EvgeniyRibakov <evgenijrybakov01@gmail.com>
2024-12-09 14:19:38 +0300

    Initial commit: Изменил расположение файлов

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from catalog.newapp.apps import NewappConfig
-from catalog.newapp.views import home, contacts
+from newapp.apps import NewappConfig
+from newapp.views import home, contacts
 
 app_name = NewappConfig.name
 
