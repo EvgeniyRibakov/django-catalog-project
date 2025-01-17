3cb84038a04beb72ae0446cbbc7c9e9dc397141d
EvgeniyRibakov <evgenijrybakov01@gmail.com>
2025-01-17 16:04:40 +0300

    Initial commit: 26.1 first check

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,17 +2,16 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import (HomeTemplateView, ContactTemplateView, ProductDetailView, ProductListView, ProductCreateView,
                            ProductUpdateView, ProductDeleteView)
-from django.conf import settings
-from django.conf.urls.static import static
+from . import views
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', HomeTemplateView.as_view(), name='home'),
+    path('', HomeTemplateView.as_view(), name='home'),
     path('contacts/', ContactTemplateView.as_view(), name='contacts'),
     path('product_list/', ProductListView.as_view(), name='product_list'),
-    path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('products/create', ProductCreateView.as_view(), name='product_create'),
-    path('products/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    path('products/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', views.ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', views.ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
 ]
