50f0a8d3def53e3998520ce133ec90946de56d90
EvgeniyRibakov <evgenijrybakov01@gmail.com>
2025-02-26 18:49:38 +0300

    28 add catalog schablons

--- catalog/views.py
+++ catalog/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
-from catalog.forms import ProductForms, ProductModeratorForms  # Исправлена ошибка в импорте
+from catalog.forms import ProductForms, ProductModeratorForms
 from catalog.models import Product
 
 
@@ -30,7 +30,7 @@ class ProductDetailView(DetailView):
 
 
 # Создание нового продукта
-class ProductCreateView(CreateView, LoginRequiredMixin):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     form_class = ProductForms
     template_name = 'catalog/product_form.html'
@@ -53,17 +53,36 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
     def get_success_url(self):
         return reverse_lazy('catalog:product_list')
 
+    def get_object(self, queryset=None):
+        # Получаем объект продукта
+        obj = super().get_object(queryset)
+        # Проверяем, является ли текущий пользователь владельцем
+        if obj.owner != self.request.user:
+            # Если не владелец и нет специальных разрешений, запрещаем доступ
+            if not (self.request.user.has_perm("catalog.can_edit_product") and
+                    self.request.user.has_perm("catalog.can_edit_description")):
+                raise PermissionDenied("Вы не можете редактировать этот продукт.")
+        return obj
+
     def get_form_class(self):
         user = self.request.user
         if user == self.object.owner:
             return ProductForms
-        if user.has_perm("catalog.can_edit_product") and user.has_perm("catalog.can_edit_descriptions"):
+        if user.has_perm("catalog.can_edit_product") and user.has_perm("catalog.can_edit_description"):
             return ProductModeratorForms
-        raise PermissionDenied()
+        raise PermissionDenied("У вас нет прав для редактирования этого продукта.")
 
 
 # Удаление продукта
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:product_list')
+
+    def get_object(self, queryset=None):
+        # Получаем объект продукта
+        obj = super().get_object(queryset)
+        # Проверяем, является ли текущий пользователь владельцем
+        if obj.owner != self.request.user:
+            raise PermissionDenied("Вы не можете удалить этот продукт.")
+        return obj
